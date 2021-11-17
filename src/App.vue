0f1e6e355208f2dadf9ba7a69d089a0f7c309d67
JHelar <john.helarsson@gmail.com>
2021-11-17 17:35:27 +0100

    fix: updated information

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app" class="cv">
-    <cv-header v-bind="{...headerProps}" />
+    <cv-header v-bind="{ ...headerProps }" />
     <main class="cv-content">
-      <cv-section v-for="section in sections" :key="section.title" v-bind="{...section}"/>
+      <cv-section
+        v-for="section in sections"
+        :key="section.title"
+        v-bind="{ ...section }"
+      />
     </main>
   </div>
 </template>
 
 <script>
-import CvHeader from './components/CvHeader';
-import CvSection from './components/CvSection';
+import CvHeader from "./components/CvHeader";
+import CvSection from "./components/CvSection";
 
 export default {
   name: "app",
@@ -19,158 +23,190 @@ export default {
   },
   data() {
     return {
-      name: 'John',
-      surname: 'Larsson',
-      workTitle: 'Frontend / Web Developer',
+      name: "John",
+      surname: "Larsson",
+      workTitle: "Engineer / Frontend Developer",
       contactInfo: [
         {
-          icon: 'fas fa-map-marker-alt',
-          text: 'Oskarstorget 16D Örebro, 70214',
+          icon: "fas fa-map-marker-alt",
+          text: "Ola Hanssonsgatan 9, 11252 Stockholm",
           link: undefined
         },
         {
-          icon: 'fas fa-phone',
-          text: '+46 739 692 18',
-          link: 'tel:+4673969218'
+          icon: "fas fa-phone",
+          text: "+46 739 692 18",
+          link: "tel:+4673969218"
         },
         {
-          icon: 'fas fa-envelope',
-          text: 'john.helarsson@gmail.com',
-          link: 'mailto:john.helarsson@gmail.com'
+          icon: "fas fa-envelope",
+          text: "john.helarsson@gmail.com",
+          link: "mailto:john.helarsson@gmail.com"
         },
         {
-          icon: 'fab fa-linkedin',
-          text: 'https://www.linkedin.com/in/john-larsson-8ba758b3/',
-          link: 'https://www.linkedin.com/in/john-larsson-8ba758b3/'
+          icon: "fab fa-linkedin",
+          text: "https://www.linkedin.com/in/john-larsson-8ba758b3/",
+          link: "https://www.linkedin.com/in/john-larsson-8ba758b3/"
         }
       ],
       sections: [
         {
-          title: 'Skills',
-          icon: 'fas fa-puzzle-piece',
+          title: "Experience",
+          icon: "fas fa-briefcase",
           rows: [
             {
-              title: undefined,
-              subTitle: undefined,
-              content: 'Javascript, Typescript'
+              title: "Javascript Engineer (Level 3 - Senior), Klarna",
+              subTitle: "2020 - ",
+              content: `
+                Engineer in a team of 5 which handles all Klarna CRM channels, such as: in app messages, in app articles, native push notifications, sms and email.<br><br>
+                Enabeling and supporting marketeers with campaign setup and delivery.<br>
+                Writing and maintaining backend services in express, Node.js and Postgress. With an AWS tech stack. <br>
+                Writing the frontend for the production app on both web and native platforms using technologies such as react and react-native with a GraphQL/JSON-rest api. 
+              `
             },
             {
-              title: undefined,
-              subTitle: undefined,
-              content: 'Node.js'
-            },
-            {
-              title: undefined,
-              subTitle: undefined,
-              content: 'React.js, Vue.js'
-            },
-            {
-              title: undefined,
-              subTitle: undefined,
-              content: 'Sass, Less, CSS'
-            },
-            {
-              title: undefined,
-              subTitle: undefined,
-              content: 'HTML'
-            }
-          ]
-        },
-        {
-          title: 'Experience',
-          icon: 'fas fa-briefcase',
-          rows: [
-            {
-              title: 'Web Developer, Consultant',
-              subTitle: 'HiQ Örebro, Örebro',
+              title: "Web Developer - Consultant, HiQ",
+              subTitle: "2015 - 2020",
               content: `Involved in numerous web projects for both public and private sector, as a lead web developer and systems architect.<br>
 Created an inhouse tool to increase the speed of development, deployment and most of all developer enjoyment of projects for the content management system, Sitevision.`
             },
             {
-              title: 'Developer Intern',
-              subTitle: 'Digital Information, Örebro',
+              title: "Developer Intern, Digital Information",
+              subTitle: "2015 - 2015",
               content: `University exam project.<br>
 Researched and surveyed existing users of a web booking platform.<br>
 With the user data gathered from the surveys a new payment function was developed for the website.`
             }
           ]
         },
+
         {
-          title: 'Projects',
-          icon: 'fas fa-project-diagram',
+          title: "Projects",
+          icon: "fas fa-project-diagram",
           rows: [
             {
-              title: undefined,
-              subTitle: undefined,
-              content: `svhiq, CLI tool for creation, development and deployment of Sitevision web projects. GitHub: <a href="https://github.com/JHelar/svb">https://github.com/JHelar/svb</a>`
+              title: "In app articles rework, Klarna",
+              subTitle: "Engineer",
+              content: `Lead engineer to the rework of the in app articles in the Klarna native and web app. Worked tighlty with designer to come up with and rework the existing articles.<br><br>
+              Project involved extending the existing CMS editor to be HEAD-less. Adding a graphql api to fetch data from to be later rendered within the app. Added native in app rendering of articles for both the native and web platform.<br><br>
+              <b>Released publicly October 2021.</b>`
             },
             {
-              title: undefined,
-              subTitle: undefined,
-              content: `match-3, a match-3 (candy crush inspired) game made for the web browser, GitHub: <a href="https://github.com/JHelar/match-3">https://github.com/JHelar/match-3</a>`
+              title: "In App Mesages, Klarna",
+              subTitle: "Engineer",
+              content: `
+                Lead engineer in the project to add natively rendered in app messages to the Klarna app on both native and web platforms.<br><br>
+                Enabeling marketeers to remotely render messages directly within the application it self without worrying about the visuals in the app.<br><br>
+                Project involved extending the allready existing native api's and pushing the data to the react-native frontend to be rendered using the applications visual components.<br><br>
+                <b>Released publicly Mars 2020.</b>
+              `
+            },
+            {
+              title: "svhiq, HiQ",
+              subTitle: "Engineer",
+              content: `CLI tool for creation, development and deployment of Sitevision web projects. GitHub: <a href="https://github.com/JHelar/svb">https://github.com/JHelar/svb</a><br>Still heavily being used in projects.`
+            },
+            {
+              title: "match-3",
+              subTitle: "Personal",
+              content: `A match-3 (candy crush inspired) game made for the web browser, GitHub: <a href="https://github.com/JHelar/match-3">https://github.com/JHelar/match-3</a>`
             }
           ]
         },
         {
-          title: 'References',
-          icon: 'fas fa-user-tie',
+          title: "References",
+          icon: "fas fa-user-tie",
           rows: [
             {
-              title: 'Kungliga biblioteket',
-              subTitle: 'Anders Hejl',
-              content: `<a href="mailto:anders.hejl@kb.se">anders.hejl@kb.se</a>`
+              title: "Matteo Pitassi",
+              subTitle: "Klarna",
+              content: `Cooworker, worked tighlty with on reworking the in app articles for Klarna: <a href="mailto:matteo.pitassi@klarna.com">matteo.pitassi@klarna.com</a>`
             },
             {
-              title: 'HiQ Örebro',
-              subTitle: 'Anna Östh',
-              content: `<a href="mailto:anna.osth@hiq.se">anna.osth@hiq.se</a>`
+              title: "Viktor Söderström",
+              subTitle: "HiQ",
+              content: `Cooworker in the same team at HiQ: <a href="mailto:viktor.soderstrom@hiq.se">viktor.soderstrom@hiq.se</a>`
             },
-          ]
-        },
-        {
-          title: 'Certifications',
-          icon: 'fas fa-award',
-          rows: [
             {
-              title: undefined,
-              subTitle: undefined,
-              content: `Sitevision certified Website Developer`
+              title: "Johan Lundberg",
+              subTitle: "HiQ",
+              content: `Project and consultant manager at HiQ: <a href="mailto:johan.lundberg@hiq.se">johan.lundberg@hiq.se</a>`
             }
           ]
         },
         {
-          title: 'Education',
-          icon: 'fas fa-graduation-cap',
+          title: "Skills",
+          icon: "fas fa-puzzle-piece",
           rows: [
             {
-              title: 'Bachelor: Computer Science',
-              subTitle: 'Örebro Universitet - Örebro',
-              content: '85% Completed'
+              title: undefined,
+              subTitle: undefined,
+              content: "Javascript, Typescript"
             },
             {
-              title: 'Highschool: Nature Science',
-              subTitle: 'Östra Real - Stockholm',
+              title: undefined,
+              subTitle: undefined,
+              content: "Node.js"
+            },
+            {
+              title: undefined,
+              subTitle: undefined,
+              content: "express.js"
+            },
+            {
+              title: undefined,
+              subTitle: undefined,
+              content: "Postgress"
+            },
+            {
+              title: undefined,
+              subTitle: undefined,
+              content: "React.js, Vue.js"
+            },
+            {
+              title: undefined,
+              subTitle: undefined,
+              content: "Sass, Less, CSS"
+            },
+            {
+              title: undefined,
+              subTitle: undefined,
+              content: "HTML"
+            }
+          ]
+        },
+        {
+          title: "Education",
+          icon: "fas fa-graduation-cap",
+          rows: [
+            {
+              title: "Bachelor: Computer Science",
+              subTitle: "Örebro Universitet - Örebro",
+              content: "Not completed"
+            },
+            {
+              title: "Highschool: Nature Science",
+              subTitle: "Östra Real - Stockholm",
               content: undefined
             }
           ]
-        },
+        }
       ]
-    }
+    };
   },
   computed: {
-    headerProps(){
+    headerProps() {
       return {
         title: `${this.name} ${this.surname}`,
         subTitle: this.workTitle,
         contactInfo: this.contactInfo
-      }
+      };
     }
   }
 };
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap");
 
 :root {
   --color-blue: #002e58;
@@ -183,7 +219,7 @@ With the user data gathered from the surveys a new payment function was develope
   --sidebar-width: 150px;
 
   --content-padding: 1rem;
-} 
+}
 html {
   margin: 0;
 }
@@ -220,7 +256,7 @@ body {
   padding: calc(var(--content-padding) * 2) var(--content-padding);
 
   &::before {
-    content: '';
+    content: "";
     position: absolute;
     background-color: var(--color-blue);
     top: 0;
@@ -231,7 +267,12 @@ body {
 }
 
 @media print {
-  #Header, #Footer { display: none !important; }
-  @page { margin: 0; }
+  #Header,
+  #Footer {
+    display: none !important;
+  }
+  @page {
+    margin: 0;
+  }
 }
 </style>
